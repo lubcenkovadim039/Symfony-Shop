61ec988d9ed98fd75f319b255d99fade95a31257
skillup-vadim <lubcenkovadim039@gmail.com>
2018-03-30 21:21:29 +0300

    lesson16-symfony

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ProductController extends Controller
+{
+    /**
+     * @Route("/product", name ="product")
+     */
+    public function index()
+    {
+        $repo = $this->getDoctrine()->getRepository(Product::class);
+        $products = $repo->findAll();
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+
+    /**
+     * @Route("/product/create", name="product_create")
+     */
+    public function create()
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $product = new Product();
+        $product->setTitle('Телевизор');
+        $product->setPrice(10000);
+        $product->setDescription('Большой телевизор');
+
+        $entityManager->persist($product);
+        $entityManager->flush();
+        return new Response('Продукт сохранен id ' . $product->getId());
+        /*
+                return $this->render('product/index.html.twig', [
+                    'controller_name' => 'ProductController',
+                ]);*/
+    }
+
+    /**
+     * @Route("/product/{id}", name="product_show")
+     */
+    public function showAction(Product $product)
+    {
+        /*  $product = $this->getDoctrine()
+              ->getRepository(Product::class)
+              ->find($id);
+
+          if (!$product) {
+              throw $this->createNotFoundException(
+                  'No product found for id '.$id
+              );
+          }*/
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+
+        /* return new Response('Продукт: '.$product->getTitle().' цена '.$product->getPrice());*/
+
+
+    }
+
+    /**
+     * @Route("/product/{id}/edit", name="product_edit")
+     */
+    public function edit(Product $product)
+    {
+        $product->setPrice($product->getPrice() + 10);
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+    /**
+     * @Route("/product/{id}/delete", name="product_delete")
+     */
+    public function delete(Product $product)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($product);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('product');
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ * @ORM\Table(name="products")
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10,scale=2, nullable=true)
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+}
